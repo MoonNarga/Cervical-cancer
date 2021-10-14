251c535f97617e4bfe26c4c0e4e8d95914323b8b
MoonNarga <MoonNarga@outlook.com>
2021-10-14 18:13:46 +0800

    update

--- get_label_image.py
+++ get_label_image.py
@@ -3,9 +3,15 @@ import os
 
 path = "E:\labelset"
 path_dir = "E:\labelset"
-sum = 0
+sum1 = 0
+
 for root, dirs, files in os.walk(path):
-    for dirname in dirs:
-        # s = 0
-        for root, subdir, fil in os.walk(os.path.join(path, dirname)):
-            print(len(fil))
+    for di in dirs:
+        if (di != "labelset"):
+            for ro, d, fil in os.walk(os.path.join(root, di)):
+                for f in fil:
+                    if (f == "0000.json"):
+                        sum1 += 1
+                        id = ro[12:]                        
+                        os.system("labelme_json_to_dataset \"" + os.path.join(ro, f) + "\" -o " + "\"E:/iodine/" + id + '\"')
+print(sum1)
