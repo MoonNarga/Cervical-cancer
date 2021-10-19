abd0b5f59c18222118123f29f775832a5cdbf353
MoonNarga <MoonNarga@outlook.com>
2021-10-19 14:57:10 +0800

    update

--- get_jsoninfo.py
+++ get_jsoninfo.py
@@ -7,7 +7,10 @@ path = "E:/labelset"
 for root, dirs, files in os.walk(path):
     for f in files:
         if (f[-4:] == "json" and f[:3] != "fix"):
-            fp = open(os.path.join(root, f), encoding="utf-8")
-            js = json.load(fp)
-            del js["imageData"]
-            print(js)
+            try:
+                with open(os.path.join(root, f), 'r', encoding="utf-8") as fp:
+                    content = fp.read()
+                with open(os.path.join(root, f), 'w', encoding="gbk") as fp:
+                    fp.write(content)
+            except UnicodeDecodeError as e:
+                pass
