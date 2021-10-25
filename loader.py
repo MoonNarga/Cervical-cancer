1fa741884ae939e8d85418445fb78da8a6dcf15f
MoonNarga <MoonNarga@outlook.com>
2021-10-25 15:48:52 +0800

    loader done

--- loader.py
+++ loader.py
@@ -1,24 +1,24 @@
 import paddle
 import dataset
 
-BATCH_SIZE = 64
-BATCH_NUM = 20
+BATCH_SIZE = 20
+BATCH_NUM = 4
 
 # 测试定义的数据集
-custom_dataset = dataset.IodineDataset()
+custom_dataset = dataset.AceticDataset()
 
-print('=============custom dataset=============')
-for data, label in custom_dataset:
-    data.show()
-    label.show()
-    break
-
-# train_loader = paddle.io.DataLoader(custom_dataset, batch_size=BATCH_SIZE, shuffle=True)
-# # 如果要加载内置数据集，将 custom_dataset 换为 train_dataset 即可
-# for batch_id, data in enumerate(train_loader()):
-#     x_data = data[0]
-#     y_data = data[1]
-
-#     print(x_data.shape)
-#     print(y_data.shape)
+# print('=============custom dataset=============')
+# for data, label in custom_dataset:
+#     data.show()
+#     label.show()
 #     break
+
+train_loader = paddle.io.DataLoader(custom_dataset, places=paddle.CUDAPlace(0), batch_size=BATCH_SIZE, shuffle=True)
+# 如果要加载内置数据集，将 custom_dataset 换为 train_dataset 即可
+for batch_id, data in enumerate(train_loader()):
+    x_data = data[0]
+    y_data = data[1]
+
+    print(x_data)
+    print(y_data)
+    # break
