4c745b8bca4425b8923173fa3c537e5100800b7e
MoonNarga <MoonNarga@outlook.com>
2021-11-01 16:26:03 +0800

    update

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from matplotlib.pyplot import pause
 import paddle.vision.datasets as s
+import time
 a = s.Cifar10()
+for i in range(10):
+    d = a[i][0]
+    d.show()
+    print(a[i][1])
+    time.sleep(3)
+    d.close()
+    
